4bc23ff1b22fe87e44bd65080e6ecd4f908ade49
GodDamnVel <ve23@njit.edu>
2015-03-29 15:20:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
+$array = file('public_html/hd2013.csv');
 
+foreach($array as $key => $var)
+{
+   $array[$key] = explode(',' , $var);
+}
 
+echo '<table class ="samples">';
+foreach ($array as $key => $value)
+{
+  $i++;
+  echo '<tr class=d' . ($i & 1) . ' align="center">';
+  echo '<td>' . $arrary[$key][0] . </td>';
+  echo '</tr>';
+}
 
-echo 'updated';
-
+echo '</table>';
 ?>
-
